f8dceebc879d7b2130d43b9ac1422b6baeb78f4f
Volodymyr Mykychak <leadervlo.d@gmail.com>
2026-02-20 23:40:46 +0200

    implement car rental cost calculation with constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,28 @@
 /**
- * @param {number} days
+ * @param {number} rentalDays
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  // write code here
+function calculateRentalCost(rentalDays) {
+  const DAILY_RATE = 40;
+
+  const LONG_TERM_LIMIT = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  const SHORT_TERM_LIMIT = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const totalBasePrice = rentalDays * DAILY_RATE;
+
+  if (rentalDays >= LONG_TERM_LIMIT) {
+    return totalBasePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (rentalDays >= SHORT_TERM_LIMIT) {
+    return totalBasePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return totalBasePrice;
 }
 
 module.exports = calculateRentalCost;
